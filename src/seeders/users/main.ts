d82f9a93c7d912807140812bf83bf14f3d6e31df
alves-leandro <l.alves85@live.com>
2024-01-22 12:47:47 -0300

    admin seed

--- src/seeders/users/main.ts
+++ src/seeders/users/main.ts
@@ -5,7 +5,13 @@ import { randomUUID } from 'crypto';
 
 const chance = new Chance();
 
-const possibleRules = ['CLEANER', 'RECEPTIONIST', 'KITCHEN', 'GUEST'];
+const possibleRules = [
+  'CLEANER',
+  'RECEPTIONIST',
+  'KITCHEN',
+  'GUEST',
+  'MANAGER',
+];
 
 async function seed() {
   const passwordHash = await hash('123456seed', 10);
@@ -13,6 +19,27 @@ async function seed() {
   const prisma = new PrismaClient();
 
   try {
+    // Verificar se o usuário "admin" já existe
+    const adminUser = await prisma.user.findFirst({
+      where: {
+        name: 'admin',
+      },
+    });
+
+    if (!adminUser) {
+      // Se o usuário "admin" não existir, crie-o
+      await prisma.user.create({
+        data: {
+          id: randomUUID(),
+          name: 'admin',
+          username: 'admin',
+          email: 'admin@mail.com',
+          rule: 'MANAGER',
+          password: await hash('123', 10),
+        },
+      });
+    }
+
     for (let i = 0; i < 15; i++) {
       const name = chance.name();
       const username = name.toLowerCase().replace(/\s/g, '');
